90bc0a1420dce7f6ff2a4d0a9b5fd85f6d633941
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-11 17:41:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
-def fizzbuzz(int)
-  if int =  % 3 == 0 && %5==0
-    return = "Fizzbuzz"
-  
-  elsif int % 3 == 0 
-    return = "Fizz"
-  
-  elsif int % 5 == 0
-    return = "Buzz"
+def fizzBuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else
+    number
   end
 end
