0799c290a43401a57fd6487c2dbbe728c7ce7446
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-11 17:45:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(irb)
-  if irb % 3 == 0
-    "Fizz"
+  if  irb % 3 == 0 && irb % 5 == 0
+    "FizzBuzz"
   elsif irb % 5 == 0
     "Buzz"
-  elsif irb % 3 == 0 && irb % 5 == 0
-    "FizzBuzz"
+  elsif irb % 3 == 0
+    "Fizz"
   else
     nil
   end
